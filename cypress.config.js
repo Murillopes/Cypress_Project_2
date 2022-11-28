12cbff4f2c2ee12d1879ee211ff70bd73656883c
Murillo Lopes <murillo.lopes.oliveiraa@gmail.com>
2022-11-28 20:09:29 -0300

    inserindo documento do GithubActions

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress")
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "7g11gb",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
