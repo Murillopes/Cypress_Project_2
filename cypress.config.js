522d6a50f1c02b581531b254706e51a85e28421a
Murillo Lopes <murillo.lopes.oliveiraa@gmail.com>
2022-11-28 20:43:24 -0300

    atualização do ID

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require("cypress")
 
 module.exports = defineConfig({
+  projectId: "7g11gb",
   e2e: {
-    projectId: "7g11gb",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
